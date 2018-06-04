506cf1aeb1a926ad6ebef08385c160bee9951f15
Emily Venturino <ebventurino@gmail.com>
2018-06-04 09:11:02 -0500

    finished exercises

---
+++ lightning.js
@@ -0,0 +1,8 @@
+const lastName = "Venturino"
+for (i = 0; i < lastName.length; i++){
+    if(i <= 7) {
+        console.log(lastName)
+    } else {
+        console.log("I can't pronounce this last name")
+    }
+}
