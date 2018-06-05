23fb9a53ddb5a9627dc4987b86a821c3532dfe05
Emily Venturino <ebventurino@gmail.com>
2018-06-05 09:12:37 -0500

    exercise 2

--- lightning.js
+++ lightning.js
@@ -1,8 +1,18 @@
-const lastName = "Venturino"
-for (i = 0; i < lastName.length; i++){
-    if(i <= 7) {
-        console.log(lastName)
-    } else {
-        console.log("I can't pronounce this last name")
-    }
+// const lastName = "Venturino"
+// for (i = 0; i < lastName.length; i++){
+//     if(i <= 7) {
+//         console.log(lastName)
+//     } else {
+//         console.log("I can't pronounce this last name")
+//     }
+// }
+
+
+//                  EXERCISE 2
+
+
+ const name = function (firstName, lastName) {
+    console.log(`My name is ${firstName} ${lastName}`)
 }
+
+fullName("Emily", "Venturino")
